1a4143bd81f4ce494e94819f1caab20617dcce89
alex v <alexminga.a@gmail.com>
2022-12-28 17:34:41 -0500

    📝 docs: document the HomePaginator class

--- lib/src/features/home_page/presentation/widgets/home_paginator.dart
+++ lib/src/features/home_page/presentation/widgets/home_paginator.dart
@@ -5,6 +5,8 @@ import 'package:pokedex/src/features/home_page/presentation/widgets/my_custom_sc
 
 import '../bloc_manager/home_bloc.dart';
 
+/// The HomePaginator widget is responsible for displaying a list of Pokemon on the homepage.
+/// It uses an infinite scroll feature to allow the user to load more Pokemon as they scroll through the list.
 class HomePaginator extends StatefulWidget {
   const HomePaginator({super.key});
 
@@ -13,9 +15,16 @@ class HomePaginator extends StatefulWidget {
 }
 
 class _PaginatorState extends State<HomePaginator> {
+  /// A reference to the HomeBloc, which is used to fetch and manage the list of Pokemon
   late HomeBloc pokeBloc;
+
+  /// A flag to control when more Pokemon should be fetched
   bool scrollSwitch = true;
+
+  /// The current offset in the list of Pokemon
   int countPokemon = 0;
+
+  /// The list of all Pokemon that have been fetched so far
   List<Pokemon> allPokemonList = [];
 
   @override
@@ -26,20 +35,25 @@ class _PaginatorState extends State<HomePaginator> {
   @override
   void dispose() {
     super.dispose();
+    allPokemonList = [];
   }
 
   @override
   Widget build(BuildContext context) {
+    /// Get a reference to the HomeBloc from the context
     pokeBloc = BlocProvider.of<HomeBloc>(context);
 
     return BlocBuilder<HomeBloc, HomeState>(
       bloc: pokeBloc,
       builder: (context, state) {
+        /// If the state is HomeInitial, show a loading spinner while waiting for Pokemon to be fetched
         if (state is HomeInitial) {
+          /// Send an event to the HomeBloc to fetch the next batch of Pokemon
           pokeBloc.add(AddMorePokemons(countPokemon));
           return const Center(child: CircularProgressIndicator());
         }
 
+        /// If the state is WithPokemonsState, display the list of Pokemon
         if (state is WithPokemonsState) {
           scrollSwitch = true;
           return pokemonsPage(state);
@@ -49,11 +63,15 @@ class _PaginatorState extends State<HomePaginator> {
     );
   }
 
+  /// Returns a widget that displays the list of Pokemon
   Widget pokemonsPage(WithPokemonsState state) {
+    /// Add the new Pokemon to the list of all Pokemon
     allPokemonList.addAll(state.pokemons);
 
+    /// Create a scroll controller to enable infinite scrolling
     ScrollController scrollController = ScrollController();
     scrollController.addListener(() {
+      /// When the user has scrolled to within 200 pixels of the end of the list, fetch more Pokemon
       if (scrollController.position.pixels >=
               scrollController.position.maxScrollExtent - 200 &&
           scrollSwitch == true) {
